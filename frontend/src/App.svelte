5f39b8c58bcffb54ead1eb4b1d4c58f12f623e0b
jaewon028 <jaewon028@gmail.com>
2024-03-24 02:06:16 +0900

    frontend commit

---
+++ frontend/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  let message;
+
+  // fetch하기. FAST API 서버에
+  fetch("http://127.0.0.1:8000/hello").then((response) => {
+    response.json().then((json) => {
+      message = json.message;
+    });
+  });
+</script>
+
+<h1>{message}</h1>
